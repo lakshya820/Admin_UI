e003659c1d7585d12c5c528477f6811fd731f6f2
lakshya820 <lakshyaanand820@gmail.com>
2024-09-27 15:48:17 +0530

    Latest commit

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,8 @@ import Tests2 from './components/Tests2';
 import Learners1 from './components/Learners1';
 import Learners2 from './components/Learners2';
 import Reports from './components/Reports';
+import Tests1Admin from './components/Tests1Admin';
+import Tests2Admin from './components/Tests2Admin';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -72,9 +74,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/main" element={<MainLayout />}>
-        <Route path="tests1" element={<Tests1 />} />
-        <Route path="dashboard" element={<Dashboard2 />} />
-        <Route path="tests2" element={<Tests2 />} />
+        <Route path="tests1" element={<Tests1Admin />} />
+        <Route path="dashboard" element={<DashboardAdmin />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="reports" element={<Reports />} />
+        <Route path="learners1" element={<Learners1 />} />
+        <Route path="learners2" element={<Learners2 />} />
+        <Route path="tests2" element={<Tests2Admin />} />
       </Route>
     </Routes>
   </Router>
